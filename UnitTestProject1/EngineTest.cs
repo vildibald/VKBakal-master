793f25bd77b2a5e20c9edc455b33beaae4b8ac87
Viliam Kačala <viliam_kacala_ml@hotmail.com>
2018-01-23 22:01:06 +0100

    Add files via upload

---
+++ UnitTestProject1/EngineTest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+//using SilverlightApplication3;
+
+namespace Testing
+{
+    [TestClass]
+    public class EngineTest
+    {
+      
+  //      private double plotAreaMin_X = 0;
+  //      private double plotAreaMax_X = 775;
+  //      private double plotAreaMin_Y = 0;
+  //      private double plotAreaMax_Y = 575;
+  //      private double worldAreaMin_X = -10;
+  //      private double worldAreaMax_X = 10;
+  //      private double worldAreaMin_Y = -10;
+  //      private double worldAreaMax_Y = 10;
+  ////      private Engine engine;
+
+  //      [TestInitialize]
+  //      public EngineTest()
+  //      {
+  //          //engine = new Engine(new Canvas, 20, 20, plotAreaMin_X, plotAreaMin_Y, plotAreaMax_X, plotAreaMax_Y,
+  //            //  worldAreaMin_X, worldAreaMin_Y, worldAreaMax_X, worldAreaMax_Y);
+  //      }
+
+        
+  //      [TestMethod]
+  //      public void generate_bell_test()
+  //      {
+  //      }
+    }
+}
